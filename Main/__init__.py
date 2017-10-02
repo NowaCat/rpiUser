843cd1590067ebdb4614c6a3df44c7f3a045086c
NowaCat <hidy253029174@gmail.com>
2017-10-02 11:16:59 +1100

    Initial commit

---
+++ Main/__init__.py
@@ -0,0 +1,16 @@
+buffer = ""
+
+print("Welcome to Virgil!")
+print("Please enter user name:")
+while input() != "user1":
+    input("User does not exit! Please try again!\n")
+
+else:
+    print("Welcome back user1!\nYou can tpye whatever you need below!")
+    try:
+        while True:
+            input()
+            userInput= input()
+            buffer += userInput + "\n"
+    except:
+        break
